b91d1cd6aeba6afb42ea5efd2325a0c541e5e8c7
sayasha <2two2two8eight@mail.ru>
2024-12-01 22:45:22 +0300

    word wrap

--- frontend/src/components/messanger/message/Message.tsx
+++ frontend/src/components/messanger/message/Message.tsx
@@ -17,7 +17,7 @@ const Message:React.FC<IMessageProps> = ({el}) => {
 		<>
 		<div className={`flex items-center gap-4 ${el.sender.username === localStorage.getItem('registered') && 'flex-row-reverse'}`}>
 			<div className={`bg-background-400 max-w-screen-xl break-all flex-wrap py-3.5 px-4 ${el.sender.username === localStorage.getItem('registered') ? 'rounded-l-xl' : 'rounded-r-xl'} rounded-t-xl`}>
-				<p className={`${el.sender.username === localStorage.getItem('registered') ? 'text-primary' : 'text-white'} text-base font-light whitespace-pre-wrap`}>{el.content}</p>
+				<p className={`${el.sender.username === localStorage.getItem('registered') ? 'text-primary' : 'text-white'} text-base font-light break-normal overflow-wrap break-words`}>{el.content}</p>
 			</div>
 			<p className='text-subtitle-gray text-sm text-right'>{timeString}</p>
 		</div>
