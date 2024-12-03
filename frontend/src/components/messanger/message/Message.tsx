582c7fa110337b201cab6a3535793ee9ec75f1c1
sayasha <2two2two8eight@mail.ru>
2024-12-03 15:43:38 +0300

    uptd

--- frontend/src/components/messanger/message/Message.tsx
+++ frontend/src/components/messanger/message/Message.tsx
@@ -4,6 +4,7 @@ import { IMessageProps } from '../../../types/props/types.props'
 const Message:React.FC<IMessageProps> = ({el}) => {
 	const dateString = el.createdAt
 	const dateObject = new Date(dateString)
+	const currentUser = localStorage.getItem('registered')
 
 	// Получение часов, минут и секунд
 	const hours = dateObject.getHours()
@@ -15,13 +16,21 @@ const Message:React.FC<IMessageProps> = ({el}) => {
 
 	return (
 		<>
-		<div className={`flex items-center gap-4 ${el.sender.username === localStorage.getItem('registered') && 'flex-row-reverse'}`}>
-			<div className={`bg-background-400 max-w-screen-xl ${el.sender.username === localStorage.getItem('registered') ? 'rounded-l-xl' : 'rounded-r-xl'} rounded-t-xl`}>
-				<p className={`${el.sender.username === localStorage.getItem('registered') ? 'text-primary' : 'text-white'} text-base font-light my-3.5 mx-4 text`}>
+		{/* <div className={`flex items-center gap-4 ${el.sender.username === localStorage.getItem('registered') && 'flex-row-reverse'}`}>
+			<div className={`bg-background-400 max-w-screen-xl  py-3.5 px-4 ${el.sender.username === localStorage.getItem('registered') ? 'rounded-l-xl' : 'rounded-r-xl'} rounded-t-xl`}>
+				<p className={`${el.sender.username === localStorage.getItem('registered') ? 'text-primary' : 'text-white'} text-base font-light text`}>
 						{el.content}
 				</p>
 			</div>
 		<p className='text-subtitle-gray text-sm text-right'>{timeString}</p>
+		</div> */}
+		<div className={`flex items-center gap-4 ${el.sender.username === currentUser && 'flex-row-reverse'}`}>
+			<div className={`bg-background-400 width py-3.5 px-4 flex flex-col ${el.sender.username === localStorage.getItem('registered') ? 'rounded-l-xl' : 'rounded-r-xl'} rounded-t-xl`}>
+				<p className={`${el.sender.username === currentUser ? 'text-primary' : 'text-white'} text-base font-light text relative`}>
+						{el.content}
+				</p>
+			<p className='text-subtitle-gray text-sm text-right'>{timeString}</p>
+			</div>
 		</div>
 		</>
 	)
